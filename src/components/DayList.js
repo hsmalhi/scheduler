1081d8611e05112cd0f6bd949d18ee83e5ddb8a4
Harjot Malhi <HarjotMalhi@Harjots-MacBook-Pro.local>
2019-09-03 09:23:05 -0400

    Add completed DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,14 +1,24 @@
 import React from "react";
 
-import classNames from "classnames";
+import DayListItem from "./DayListItem";
+
+// import classNames from "classnames";
 
 export default function DayList(props) {
-  let dayListClass = classNames("day-list", {
-    
-  });
+  const dayListItem = function(day, setDay) {
+    return (<DayListItem
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={setDay}
+    />);
+  };
+
+  // let dayListClass = classNames("day-list", {});
 
   return (
     <ul>
+      {props.days.map(day => dayListItem(day, props.setDay))}
     </ul>
   );
 }
