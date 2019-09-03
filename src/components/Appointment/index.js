d933c04a4ac5710b1e65f22ab2c032d2a4379195
Harjot Malhi <HarjotMalhi@Harjots-MacBook-Pro.local>
2019-09-03 15:18:39 -0400

    Complete Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 
 import 'components/Appointment/styles.scss'
 
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+import { action } from "@storybook/addon-actions/dist/preview";
+
+
 export default function Appointment(props){
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/> : <Empty onAdd={action("onAdd")}/>}
+    </article>
   );
 }
