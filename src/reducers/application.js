a268f192d0233572f65c8882abb2cf9a1f876eb6
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-09-25 13:39:43 +0000

    Break a passing test on purpose to see output in circleci

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -17,9 +17,9 @@ const updateSpots = function(state, decrease) {
 
 export default function reducer(state, action) {
   switch (action.type) {
-    case SET_DAY: {
-      return { ...state, day: action.day };
-    }
+    // case SET_DAY: {
+    //   return { ...state, day: action.day };
+    // }
     case SET_APPLICATION_DATA: {
       return {
         ...state,
